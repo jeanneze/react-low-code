888d052bbc5f70a4dbd3c6590012711f415edcb2
zhaojing834 <zhaojing834@jd.com>
2023-06-19 10:55:34 +0800

    chore: 新增commitlint插件

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zhaojing834
+ * @Date: 2023-06-19 10:50:44
+ * @LastEditors: zhaojing834
+ * @LastEditTime: 2023-06-19 10:51:42
+ * @FilePath: /react-low-code/commitlint.config.js
+ * @Descripttion: commitlint 配置
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] }
